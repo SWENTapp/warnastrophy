baaf7b639e35465cb175c106f626e371fb707724
86 <116442355+eightysix-86@users.noreply.github.com>
2025-10-10 01:18:43 +0200

    test: test Loading component to check if displayed

--- app/src/main/java/com/github/warnastrophy/core/ui/components/Loading.kt
+++ app/src/main/java/com/github/warnastrophy/core/ui/components/Loading.kt
@@ -6,13 +6,20 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+
+object LoadingTestTags {
+  const val LOADING_INDICATOR = "loadingIndicator"
+}
 
 @Composable
 fun Loading(modifier: Modifier = Modifier) {
-  Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-    CircularProgressIndicator()
-  }
+  Box(
+      modifier = modifier.fillMaxSize().testTag(LoadingTestTags.LOADING_INDICATOR),
+      contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
+      }
 }
 
 @Preview(showBackground = true)
