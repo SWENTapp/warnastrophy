e285ab2cb25dc6a9b4a66229fe7b204356e43a5f
ssidimoh <anas.sidi.mohamed@gmail.com>
2025-10-15 13:24:39 +0200

    feat: Location helpers function
    add methods to compute rectangle around a location,
    it takes into account curvature of the earth

--- app/src/main/java/com/github/warnastrophy/core/model/util/Location.kt
+++ app/src/main/java/com/github/warnastrophy/core/model/util/Location.kt
@@ -1,19 +1,78 @@
 package com.github.warnastrophy.core.model.util
 
 import com.google.android.gms.maps.model.LatLng
+import kotlin.div
+import kotlin.math.cos
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.text.toInt
+import kotlin.times
+import org.checkerframework.checker.units.qual.km
 
 data class Location(val latitude: Double, val longitude: Double, val name: String? = null) {
 
   companion object {
-    private const val earthRadiusKm = 6371.009
+    const val earthRadiusKm = 6371.009
+
+    val kmBetweeenPoints = 500
 
     fun toLatLng(location: Location): LatLng {
       return LatLng(location.latitude, location.longitude)
     }
-  }
-}
 
-fun debugPrint(msg: String) {
-  val file = java.io.File("debug.txt")
-  file.appendText(msg + "\n")
+    fun kmToLatVariation(km: Double): Double {
+      return Math.toDegrees((km / earthRadiusKm))
+    }
+
+    fun kmToLonVariation(km: Double, latitude: Double): Double {
+      val latRad = Math.toRadians(latitude)
+      val localRadius = 2 * Math.PI * earthRadiusKm * cos(latRad)
+      if (km > localRadius) {
+        return 360.0 // entire longitude range
+      }
+      return Math.toDegrees(km / earthRadiusKm)
+    }
+
+    fun getPolygon(center: Location, kmLon: Double, kmLat: Double): List<Location> {
+      val nbrLatPoints = kotlin.math.ceil((kmLat / 2.0) / kmBetweeenPoints).toInt()
+
+      val latVar = kmToLatVariation(kmLat)
+      val latVarFrag = (latVar / 2.0) / nbrLatPoints
+
+      val listOfLocs = mutableListOf<Location>()
+
+      for (i in 0..nbrLatPoints * 2) {
+        val currentLat = normalizeLat(center.latitude - latVar / 2.0 + i * latVarFrag)
+        val currentLonVar = kmToLonVariation(kmLon, currentLat) / 2.0
+        val lonPair =
+            listOf(
+                normalizeLon(center.longitude - currentLonVar),
+                normalizeLon(center.longitude + currentLonVar))
+
+        if (!listOfLocs.any { it.latitude == currentLat && it.longitude == lonPair.min() }) {
+          listOfLocs.add(Location(currentLat, lonPair.min()))
+        }
+        if (!listOfLocs.any { it.latitude == currentLat && it.longitude == lonPair.max() }) {
+          listOfLocs.add(Location(currentLat, lonPair.max()))
+        }
+      }
+      return listOfLocs
+    } // check radius and pole edge case
+
+    fun normalizeLat(lat: Double): Double {
+      return when {
+        lat > 90.0 -> 90.0
+        lat < -90.0 -> -90.0
+        else -> lat
+      }
+    }
+
+    fun normalizeLon(lon: Double): Double {
+      return when {
+        lon > 180 -> lon - 360
+        lon < -180 -> lon + 360
+        else -> lon
+      }
+    }
+  }
 }
---
+++ app/src/test/java/com/github/warnastrophy/core/model/util/LocationTest.kt
@@ -0,0 +1,80 @@
+package com.github.warnastrophy.core.model.util
+
+import com.github.warnastrophy.core.model.util.Location.Companion.earthRadiusKm
+import kotlin.compareTo
+import kotlin.math.abs
+import kotlin.rem
+import kotlin.text.get
+import org.junit.Assert.*
+import org.junit.Test
+
+class LocationTest {
+  fun varLatToKm(latVar: Double): Double {
+    return Math.toRadians(latVar) * Location.earthRadiusKm
+  }
+
+  fun varLonToKm(lonVar: Double, latitude: Double): Double {
+    val latRad = Math.toRadians(latitude)
+    return Math.toRadians(lonVar) * (Location.earthRadiusKm * Math.cos(latRad))
+  }
+
+  fun formatBoundingBoxKeeneSite(polygon: List<Location>): String {
+    val pairs = polygon.filterIndexed { index, _ -> index % 2 == 0 }
+    val impairs = polygon.filterIndexed { index, _ -> index % 2 != 0 }.reversed()
+    val orderedpoly = (pairs + impairs)
+    return buildString {
+      orderedpoly.forEachIndexed { i, loc ->
+        append("${loc.longitude}, ${loc.latitude}")
+        append("\n")
+      }
+    }
+  }
+
+  @Test
+  fun testGetBoundingBox() {
+    val location = Location(85.0, 0.0, "")
+    val polygon = Location.getPolygon(location, 5000.0, 1000.0)
+    debugPrint(formatBoundingBoxKeeneSite(polygon))
+    assertNotNull(polygon)
+  }
+
+  @Test
+  fun `equator 1000km x 1000km`() {
+    val location = Location(0.0, 0.0, "Equator")
+    val polygon = Location.getPolygon(location, 1000.0, 1000.0)
+    assertNotNull(polygon)
+    val pairs = polygon.filterIndexed { index, _ -> index % 2 == 0 }
+    val impairs = polygon.filterIndexed { index, _ -> index % 2 != 0 }.reversed()
+
+    for (i in pairs.indices) {
+      val lonVar = abs(impairs[i].longitude - pairs[i].longitude)
+      val lat = pairs[i].latitude
+      val lonKm = varLonToKm(lonVar, lat)
+      assertTrue(abs(lonKm - 1000.0) < 10.0)
+    }
+
+    val maxLat = pairs.maxOf { it.latitude }
+    val minLat = pairs.minOf { it.latitude }
+    val latVar = maxLat - minLat
+    val latKm = varLatToKm(latVar)
+    debugPrint("LatVar: $latVar, LatKm: $latKm")
+    assertTrue(abs(latKm - 1000.0) < 1.0)
+  }
+
+  @Test
+  fun `pole nord 1000km x 1000km`() {
+    val location = Location(85.0, 0.0, "Pole Nord")
+    val polygon = Location.getPolygon(location, 5000.0, 100.0)
+    assertNotNull(polygon)
+    val pairs = polygon.filterIndexed { index, _ -> index % 2 == 0 }
+    val impairs = polygon.filterIndexed { index, _ -> index % 2 != 0 }.reversed()
+
+    for (i in pairs.indices) {
+      val lonVar = abs(impairs[i].longitude - pairs[i].longitude)
+      assertEquals(360.0, lonVar, 1.0)
+    }
+  }
+}
+
+// https://www.keene.edu/campus/maps/tool/ keene website
+// test rectangle on map
