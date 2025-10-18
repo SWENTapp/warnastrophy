fb6b69b8e5566db66012e6034c745350b6add784
ssidimoh <anas.sidi.mohamed@gmail.com>
2025-10-18 14:31:48 +0200

    feat: add GpsService
    new class with a position state
    so that gps logic is extracted from MapViewModel. Code more modular

--- app/src/main/java/com/github/warnastrophy/core/model/util/AppConfig.kt
+++ app/src/main/java/com/github/warnastrophy/core/model/util/AppConfig.kt
@@ -1,5 +1,7 @@
 package com.github.warnastrophy.core.model.util
 
+import com.google.android.gms.maps.model.LatLng
+
 /**
  * `AppConfig` holds configuration settings for the application, such as the fetch delay in
  * milliseconds.
@@ -7,4 +9,8 @@ package com.github.warnastrophy.core.model.util
 object AppConfig {
   /** Fetch delay in milliseconds for periodic data updates. Default is 5000 ms (5 seconds). */
   var fetchDelayMs: Long = 5_000L
+
+  var positionUpdateDelayMs: Long = 2_000L
+
+  var defaultPosition = LatLng(18.5944, -72.3074) // Default to Port-au-Prince
 }
