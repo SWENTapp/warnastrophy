aff3ddea5d1df3994666811f701919c9be32f1bd
ssidimoh <anas.sidi.mohamed@gmail.com>
2025-10-18 18:40:21 +0200

    fix: fix health card classes imports

--- app/src/main/java/com/github/warnastrophy/core/ui/healthcard/HealthCardViewModel.kt
+++ app/src/main/java/com/github/warnastrophy/core/ui/healthcard/HealthCardViewModel.kt
@@ -3,9 +3,9 @@ package com.github.warnastrophy.core.ui.healthcard
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.github.warnastrophy.core.model.util.HealthCard
-import com.github.warnastrophy.core.model.util.HealthCardStorage
-import com.github.warnastrophy.core.model.util.StorageResult
+import com.github.warnastrophy.core.data.local.HealthCardStorage
+import com.github.warnastrophy.core.data.local.StorageResult
+import com.github.warnastrophy.core.model.HealthCard
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
