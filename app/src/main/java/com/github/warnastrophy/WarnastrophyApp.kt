cddd70a3f6a6975bd5c0d12c0abb493a0640553a
Francesco Dal Peraro <138398142+frakdp@users.noreply.github.com>
2025-10-29 21:19:19 +0100

    Fixed UI not displaying world hazards (#95)
    * fix: Fix hazards not displaying properly
    
    Fix finished, we now use the world polygon

--- app/src/main/java/com/github/warnastrophy/WarnastrophyApp.kt
+++ app/src/main/java/com/github/warnastrophy/WarnastrophyApp.kt
@@ -11,7 +11,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.github.warnastrophy.core.data.repository.HazardsRepository
+import com.github.warnastrophy.core.data.repository.HazardRepositoryProvider
 import com.github.warnastrophy.core.model.GpsService
 import com.github.warnastrophy.core.model.HazardsService
 import com.github.warnastrophy.core.ui.home.HomeScreen
@@ -38,8 +38,8 @@ fun WarnastrophyApp() {
   val locationClient = LocationServices.getFusedLocationProviderClient(LocalContext.current)
   val gpsService = GpsService(locationClient)
 
-  val hazardsRepository = HazardsRepository()
-  val hazardsService: HazardsService = HazardsService(hazardsRepository, gpsService)
+  val hazardsRepository = HazardRepositoryProvider.repository
+  val hazardsService = HazardsService(hazardsRepository, gpsService)
   Scaffold(
       bottomBar = { BottomNavigationBar(currentScreen, navController) },
       topBar = { TopBar(currentScreen) }) { innerPadding ->
