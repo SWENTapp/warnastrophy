3c1141125af5c58d08f2939a6b55855e17a78172
Valink16 <valuthy.karunakaran@epfl.ch>
2025-10-31 00:34:47 +0100

    fix: add missing fields from merge

--- app/src/androidTest/java/com/github/warnastrophy/core/ui/map/HazardCheckerTest.kt
+++ app/src/androidTest/java/com/github/warnastrophy/core/ui/map/HazardCheckerTest.kt
@@ -29,6 +29,7 @@ class HazardCheckerTest {
         Hazard(
             id = 1001, // Unique ID
             type = "TC",
+            description = "Bla bla",
             country = "Testland",
             date = "2025-01-01",
             severity = 2.0,
@@ -43,6 +44,7 @@ class HazardCheckerTest {
         Hazard(
             id = 1002, // Unique ID
             type = "FL",
+            description = "Ble ble",
             country = "Testland",
             date = "2025-01-01",
             severity = 1.0,
@@ -81,6 +83,7 @@ class HazardCheckerTest {
         Hazard(
             id = 1001,
             type = "TC",
+            description = "Bla bla",
             country = "Testland",
             date = "2025-01-01",
             severity = 2.0,
@@ -114,6 +117,7 @@ class HazardCheckerTest {
             id = 1001,
             alertLevel = 3,
             type = "TC",
+            description = "Bla bla",
             country = "Testland",
             date = "2025-01-01",
             severity = 2.0,
@@ -148,6 +152,7 @@ class HazardCheckerTest {
             id = 1001,
             alertLevel = 3,
             type = "TC",
+            description = "Bla bla",
             country = "Testland",
             date = "2025-01-01",
             severity = 2.0,
