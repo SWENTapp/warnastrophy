989f3e9db91dff9c096f0741424d5acb4dc2155d
Nguyen Ninh Dao <nguyen.dao@epfl.ch>
2025-11-17 23:48:05 +0100

    fix: comment Hilt annotation from viewModel

--- app/src/main/java/com/github/warnastrophy/core/ui/common/GlobalErrorViewModel.kt
+++ app/src/main/java/com/github/warnastrophy/core/ui/common/GlobalErrorViewModel.kt
@@ -2,12 +2,9 @@ package com.github.warnastrophy.core.ui.common
 
 import androidx.lifecycle.ViewModel
 import com.github.warnastrophy.core.domain.error.ErrorDisplayManager
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.StateFlow
 
-@HiltViewModel
-class GlobalErrorViewModel @Inject constructor(private val errorHandler: ErrorDisplayManager) :
-    ViewModel() {
+// @HiltViewModel TODO: uncomment in next PR
+class GlobalErrorViewModel(private val errorHandler: ErrorDisplayManager) : ViewModel() {
   val errorState: StateFlow<ErrorState> = errorHandler.errorState
 }
--- app/src/main/java/com/github/warnastrophy/core/ui/features/dashboard/LatestNewsViewModel.kt
+++ app/src/main/java/com/github/warnastrophy/core/ui/features/dashboard/LatestNewsViewModel.kt
@@ -3,14 +3,11 @@ package com.github.warnastrophy.core.ui.features.dashboard
 import androidx.lifecycle.ViewModel
 import com.github.warnastrophy.core.domain.model.FetcherState
 import com.github.warnastrophy.core.domain.model.HazardsDataService
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.StateFlow
 
 /** ViewModel responsible for providing the latest news and hazard data state to the UI. */
-@HiltViewModel
-class LatestNewsViewModel @Inject constructor(private val hazardsService: HazardsDataService) :
-    ViewModel() {
+// @HiltViewModel TODO: uncomment in next PR
+class LatestNewsViewModel(private val hazardsService: HazardsDataService) : ViewModel() {
   /** Exposes the fetcherState flow from the service for the UI to collect */
   val fetcherState: StateFlow<FetcherState> = hazardsService.fetcherState
 }
