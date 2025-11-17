706013cc5398f9c8218e2b5a87578bd175a01367
86 <116442355+eightysix-86@users.noreply.github.com>
2025-11-17 15:29:00 +0100

    docs: add docs to SmsService.kt

--- app/src/main/java/com/github/warnastrophy/core/domain/model/SmsService.kt
+++ app/src/main/java/com/github/warnastrophy/core/domain/model/SmsService.kt
@@ -4,10 +4,18 @@ import android.content.Context
 import android.os.Build
 import android.telephony.SmsManager
 
+/** Defines the contract for a service that sends SMS messages. */
 interface SmsSender {
+  /**
+   * Sends an SMS message to the specified phone number.
+   *
+   * @param phoneNumber The destination phone number.
+   * @param message The [EmergencyMessage] to be sent.
+   */
   fun sendSms(phoneNumber: String, message: EmergencyMessage)
 }
 
+/** An implementation of [SmsSender] that uses Android's [SmsManager]. */
 class SmsManagerSender(context: Context) : SmsSender {
   private val smsManager =
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
