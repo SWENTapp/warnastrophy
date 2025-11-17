5689504f264a3a9c7bd46aefb9f1a97126245fa7
Nguyen Ninh Dao <nguyen.dao@epfl.ch>
2025-11-17 23:22:46 +0100

    fix: assemble error

--- app/src/main/java/com/github/warnastrophy/WarnastrophyApp.kt
+++ app/src/main/java/com/github/warnastrophy/WarnastrophyApp.kt
@@ -3,6 +3,4 @@ package com.github.warnastrophy
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-class WarnastrophyApp {
-  @HiltAndroidApp class WarnastrophyApp : Application() {}
-}
+@HiltAndroidApp class WarnastrophyApp : Application() {}
