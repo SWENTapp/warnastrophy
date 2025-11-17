67898061cf6ae2ac76a52f854664c72022ad747f
Nguyen Ninh Dao <nguyen.dao@epfl.ch>
2025-11-17 21:35:59 +0100

    fix: Ensure persistent authentication
    - Replace fully qualified class names with imports statements.
    - Remove `auth.signOut()` to ensure authentication persistence.
    
    # Conflicts:
    #	app/src/main/java/com/github/warnastrophy/MainActivity.kt

--- app/src/main/java/com/github/warnastrophy/MainActivity.kt
+++ app/src/main/java/com/github/warnastrophy/MainActivity.kt
@@ -11,6 +11,8 @@ import com.github.warnastrophy.core.data.repository.HealthCardRepositoryProvider
 import com.github.warnastrophy.core.ui.theme.MainAppTheme
 import com.google.firebase.FirebaseApp
 import dagger.hilt.android.AndroidEntryPoint
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * `MainActivity` is the entry point of the application. It sets up the content view with the
@@ -29,10 +31,8 @@ class MainActivity : ComponentActivity() {
     super.onCreate(savedInstanceState)
     FirebaseApp.initializeApp(this)
 
-    val auth = com.google.firebase.auth.FirebaseAuth.getInstance()
-    val db = com.google.firebase.firestore.FirebaseFirestore.getInstance()
-
-    auth.signOut()
+    val auth = FirebaseAuth.getInstance()
+    val db = FirebaseFirestore.getInstance()
 
     HealthCardRepositoryProvider.useHybridEncrypted(applicationContext, db, auth)
     ContactRepositoryProvider.init(applicationContext)
