6615f38cc91d9117fd200ecf237cfca6d6b1ae58
Nguyen Ninh Dao <nguyen.dao@epfl.ch>
2025-11-30 12:25:44 +0100

    fix: mainActivity test

--- app/src/main/java/com/github/warnastrophy/core/ui/onboard/OnboardingScreen.kt
+++ app/src/main/java/com/github/warnastrophy/core/ui/onboard/OnboardingScreen.kt
@@ -1,25 +1,43 @@
 package com.github.warnastrophy.core.ui.onboard
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
+
+object OnboardingScreenTestTags {
+  const val NEXT_BUTTON = "NextButton"
+  const val INDICATOR = "indicator"
+}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -50,6 +68,7 @@ fun OnboardingScreen(onFinished: () -> Unit) {
             verticalAlignment = Alignment.CenterVertically) {
               Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.CenterStart) {
                 if (buttonState.value[0].isNotEmpty()) {
+                  // Back button
                   ButtonUi(
                       text = buttonState.value[0],
                       backgroundColor = Color.Transparent,
@@ -67,7 +86,9 @@ fun OnboardingScreen(onFinished: () -> Unit) {
               }
 
               Box(modifier = Modifier.weight(1f), contentAlignment = Alignment.CenterEnd) {
+                // Next button
                 ButtonUi(
+                    modifier = Modifier.testTag(OnboardingScreenTestTags.NEXT_BUTTON),
                     text = buttonState.value[1],
                     backgroundColor = MaterialTheme.colorScheme.primary,
                     textColor = MaterialTheme.colorScheme.onPrimary) {
@@ -89,4 +110,49 @@ fun OnboardingScreen(onFinished: () -> Unit) {
           }
         }
       })
+}
+
+@Composable
+fun ButtonUi(
+    text: String = "Next",
+    backgroundColor: Color = MaterialTheme.colorScheme.primary,
+    textColor: Color = MaterialTheme.colorScheme.onPrimary,
+    textStyle: TextStyle = MaterialTheme.typography.titleMedium,
+    fontSize: Int = 14,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+
+  Button(
+      modifier = modifier,
+      onClick = onClick,
+      colors =
+          ButtonDefaults.buttonColors(containerColor = backgroundColor, contentColor = textColor),
+      shape = RoundedCornerShape(10.dp)) {
+        Text(text = text, fontSize = fontSize.sp, style = textStyle)
+      }
+}
+
+@Composable
+fun IndicatorUI(
+    pageSize: Int,
+    currentPage: Int,
+    selectedColor: Color = MaterialTheme.colorScheme.secondary,
+    unselectedColor: Color = MaterialTheme.colorScheme.secondaryContainer,
+) {
+  Row(horizontalArrangement = Arrangement.SpaceBetween) {
+    repeat(pageSize) {
+      Spacer(modifier = Modifier.size(2.5.dp))
+
+      Box(
+          modifier =
+              Modifier.height(14.dp)
+                  .width(width = if (it == currentPage) 32.dp else 14.dp)
+                  .clip(RoundedCornerShape(10.dp))
+                  .background(color = if (it == currentPage) selectedColor else unselectedColor)
+                  .testTag(OnboardingScreenTestTags.INDICATOR))
+
+      Spacer(modifier = Modifier.size(2.5.dp))
+    }
+  }
 }
