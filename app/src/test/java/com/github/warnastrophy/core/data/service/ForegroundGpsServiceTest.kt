a8e9151ab44b0b93d34b2658ad487264ba064e72
Valink16 <valuthy.karunakaran@epfl.ch>
2025-12-04 05:42:57 +0100

    test: fix ForegroundGpsServiceTest to use ActivityRepositoryProvider mock and include userId

--- app/src/test/java/com/github/warnastrophy/core/data/service/ForegroundGpsServiceTest.kt
+++ app/src/test/java/com/github/warnastrophy/core/data/service/ForegroundGpsServiceTest.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.content.pm.ServiceInfo
 import android.os.Build
 import androidx.test.core.app.ApplicationProvider
+import com.github.warnastrophy.core.data.provider.ActivityRepositoryProvider
 import com.github.warnastrophy.core.domain.model.startForegroundGpsService
 import com.github.warnastrophy.core.domain.model.stopForegroundGpsService
 import com.github.warnastrophy.core.ui.common.ErrorHandler
@@ -33,6 +34,7 @@ class ForegroundGpsServiceTest {
     // Ensure ServiceStateManager is initialized for tests
     val context = ApplicationProvider.getApplicationContext<Context>()
     StateManagerService.init(context)
+    ActivityRepositoryProvider.useMock()
   }
 
   @Test
@@ -113,7 +115,9 @@ class ForegroundGpsServiceTest {
               invoked = true
               receivedContext = ctx
             },
-            stopService = { _ -> })
+            stopService = { _ -> },
+            userId = "donotcare",
+        )
 
     val context = ApplicationProvider.getApplicationContext<Context>()
     vm.onDangerModeToggled(true, context)
